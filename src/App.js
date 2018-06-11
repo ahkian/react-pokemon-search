5a0f54c85b9a0785be2938751527157aa3caf00e
myungbeans <michael.h.myung@gmail.com>
2018-06-10 21:33:34 -0400

    Started Repo

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './images/logo.svg';
+import './App.css';
+import pokelogo from './images/pokemon.png'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <img src={pokelogo} className="Pokelogo" alt="Pokemon logo"/>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to the React Pokedex</h1>
+          </div>
+        </header>
+        <body>
+
+
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
