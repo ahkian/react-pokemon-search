2b14332d2cbd727c7be5e79e5705c62af101fe45
myungbeans <michael.h.myung@gmail.com>
2018-06-10 21:43:15 -0400

    cleaning up filetree organization and rerouting src accordingly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
-import './App.css';
+import './stylesheets/App.css';
 import pokelogo from './images/pokemon.png'
 
 class App extends Component {
